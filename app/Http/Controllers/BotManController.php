f96847f851d5196bf886e11cc1aae35d331f8209
Awujoola <danielawujoola1@gmail.com>
2019-01-07 07:33:41 +0100

    initial commit

---
+++ app/Http/Controllers/BotManController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use BotMan\BotMan\BotMan;
+use Illuminate\Http\Request;
+use App\Conversations\ExampleConversation;
+
+class BotManController extends Controller
+{
+    /**
+     * Place your BotMan logic here.
+     */
+    public function handle()
+    {
+        $botman = app('botman');
+
+        $botman->listen();
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function tinker()
+    {
+        return view('tinker');
+    }
+
+    /**
+     * Loaded through routes/botman.php
+     * @param  BotMan $bot
+     */
+    public function startConversation(BotMan $bot)
+    {
+        $bot->startConversation(new ExampleConversation());
+    }
+
+    /**
+     * Handles Telegram Bot message Requests
+     *
+     * description goes here
+     *
+     * @param null
+     **/
+    public function telegram()
+    {
+        $tommy = app('botman');
+        
+        $tommy->listen();
+    }
+
+    /**
+     * Handles Facebook Bot message Requests
+     *
+     * description goes here
+     *
+     * @param null
+     **/
+    public function facebook()
+    {
+        $tommy = app('botman');
+        //add logic for facebook messenger bot
+        $this->dialogFlow();
+        
+        $tommy->listen();
+    }
+
+    public function dialogFlow(){
+
+    }
+}
