80fa8f77c8c257e6095eb3a99dbf49e6c4af0c46
Awujoola <danielawujoola1@gmail.com>
2019-01-07 16:01:05 +0100

    facebook route fix

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
 Route::match(['get', 'post'], '/botman', 'BotManController@handle');
 Route::get('/botman/tinker', 'BotManController@tinker');
 
+Route::get('/facebook', 'BotManController@facebook');
 Route::get('/telegram', 'BotManController@telegram');
