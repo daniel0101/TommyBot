e188baa27b7a68389ac9da3378eeb7a360c6d0e1
Awujoola <danielawujoola1@gmail.com>
2019-01-07 16:50:50 +0100

    Facebook bot message and replies added

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -59,7 +59,44 @@ class BotManController extends Controller
     public function facebook()
     {
         $tommy = app('botman');
+        $tommy->hears('Hi',function($tom){
+            $tom->reply("Hi my name is Tommy. What's yours");
+        });
+        $tommy->hears('{name}',function($tom,$name){
+            //save name in session Maybe?!
+            $tom->reply("Hey "+$name+". Your wish is my command");
+        });
+
+        $tommy->hears('Thank you tommy',function($tom){
+            $bot->reply(ButtonTemplate::create('Do you want to know more about 234BET?')               
+                ->addButton(ElementButton::create('Show Me the Money!')
+                    ->url('http://botman.io/')
+                )
+            );
+        });
+        $tommy->hears('Goodbye',function($tom){
+            $bot->reply(ButtonTemplate::create('Rate our service')
+                    ->addButton(ElementButton::create('Excellent')
+                        ->type('postback')
+                        ->payload('R-Excellent')
+                    )->addButton(ElementButton::create('Good')
+                        ->type('postback')
+                        ->payload('R-Good')
+                    )->addButton(ElementButton::create('Bad')
+                    ->type('postback')
+                    ->payload('R-Bad')
+                    )
+                );
+        });
+        $tommy->hears('(^R-)',function($tom,$rate){
+            $rate = explode('-',$rate);
+            $rate = $rate[1];
+            //persist rating anywhere you want
+            $tom->reply('Thank you for rating us. We are absolutely committed to serving you better');
+        });
+
         //add logic for facebook messenger bot
+        //handling and listening for user issues
         $this->dialogFlow();
         
         $tommy->listen();
