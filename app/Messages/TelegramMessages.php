e50f06cf3e3a18a10e61038de3192becf6feaca2
Awujoola <danielawujoola1@gmail.com>
2019-01-11 17:58:56 +0100

    Telegram Messages class for handling replies to messages

---
+++ app/Messages/TelegramMessages.php
@@ -0,0 +1,64 @@
+<?php 
+
+namespace App\Messages;
+
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\BotMan\Messages\Attachments\Image;
+use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
+use App\Conversations\ComplaintConversation;
+use BotMan\BotMan\Middleware\ApiAi;
+
+class TelegramMessages
+{
+
+    public function firstMessage(Botman $tommy){
+        $tommy->typesAndWaits(3);
+        $tommy->reply("Hi my name is Tommy. How can be of help to you today? \n Please Respond with **1** for Complaints \n **2** For our  latest offers. \n **3** Make we yarn wella");
+    }
+    public function nameMessage(Botman $tommy){
+        $tommy->typesAndWaits(3);
+        $tommy->reply("Hey ".$name."\n Please Respond with **1** for Complaints \n **2** For our  latest offers. \n **3** Make we yarn wella");
+    }
+
+    public function complaints(Botman $tommy){
+        $tommy->startConversation(new ComplaintConversation);
+    }
+
+    public function Offers(Botman $tommy){
+        $tommy->typesAndWaits(3);
+        //get latest offers from the admin backend
+        $offers = $this->getOffers();
+        $attachment = new Image('https://botman.io/img/logo.png');
+        // Build message object
+        $message = OutgoingMessage::create('Checkout this offers - add url here')
+                        ->withAttachment($attachment);
+        // Reply message object
+        $tommy->reply($message);
+    }
+
+    public function yarn(Botman $tommy){
+        //uses dialog flow
+        $dialogFlow = $this->initDialogFlow();
+    }
+
+    public function initDialogFlow(){
+        return ApiAi::create(env('DIALOGFLOW_API_TOKEN'))->listenForAction();
+    }
+    
+    public function initBotman(){
+        DriverManager::loadDriver(\BotMan\Drivers\Telegram\TelegramDriver::class);
+        $config = ['telegram' => [
+                'token' => '744176429:AAEsmV691fVmbm0E-qB_KqxorWF_I_uF2b8',
+            ]
+        ];
+        return BotManFactory::create($config);
+    }
+
+    public function getOffers(){
+        //get latest offers saved on the DB
+        return $offers;
+    }
+    
+}
