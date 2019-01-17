1b9e950a9beac1b27c9b5786e396ef5c179cc786
Awujoola <danielawujoola1@gmail.com>
2019-01-17 14:53:38 +0100

    new routes for admin and auth

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,16 @@ Route::get('/botman/tinker', 'BotManController@tinker');
 
 Route::match(['get','post'],'/facebook', 'BotManController@facebook');
 Route::match(['get','post'],'/telegram', 'BotManController@telegram');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin/dashboard', 'AdminController@dashboard');
+Route::get('/admin/complaints', 'AdminController@complaints');
+Route::get('/admin/complaint/{id?}', 'AdminController@complaint');
+Route::get('/admin/offers', 'AdminController@offers');
+Route::get('/admin/offer/{id?}', 'AdminController@offer');
+Route::post('/admin/offer/{id?}', 'AdminController@addOffer');
+Route::get('/admin/delete/{id}', 'AdminController@delete');
+Route::post('/admin/reply', 'AdminController@reply');
