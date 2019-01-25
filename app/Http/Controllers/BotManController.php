df5413dcb4254000d6eaecb973ff9d568191f20a
Awujoola <danielawujoola1@gmail.com>
2019-01-25 11:22:29 +0100

    handle facebook messages webhook

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -59,12 +59,9 @@ class BotManController extends Controller
         // $tommy = resolve('botman'); 
         $tommy = $this->loadConfig('telegram');
                
-        // $tommy->hears('(^Hi|Hello)',function(Botman $tom){
-        //     $tom->typesAndWaits(3);
-        //     $tom->reply("Hi my name is Tommy. How can be of help to you today?");
-        // });
         $tommy->hears('(^Hi|Hello)','App\Messages\TelegramMessages@firstMessage');
         $tommy->hears('My name is {name}','App\Messages\TelegramMessages@nameMessage');
+
         $tommy->hears('((?i)\\offers\\b)','App\Messages\TelegramMessages@Offers');
         $tommy->hears('((?i)\\offer\\b)','App\Messages\TelegramMessages@Offers');
         $tommy->hears('(^.*offers.*$)','App\Messages\TelegramMessages@Offers');
@@ -79,12 +76,6 @@ class BotManController extends Controller
 
         $tommy->hears('(^3)',function(BotMan $tommy){
             $tommy->typesAndWaits(3);
-            //make sure you move this to a class
-            $dialogflow = ApiAi::create(env('DIALOGFLOW_TOKEN'))->listenForAction();
-
-            // Apply global "received" middleware
-            $tommy->middleware->received($dialogflow);
-            
             //put cache flag for dialog flow
             Cache::put('dialogflow_flag', true, now()->addMinute(20));
             $tommy->reply("Correct! Let's go there");
@@ -116,9 +107,7 @@ class BotManController extends Controller
         //     $tom->reply($message);
         // });
 
-        $tommy->hears('(^Complaint-)',function(Botman $tom){
-            $tom->reply('I just recorded your complaint, you would be contacted shortly');
-        });
+        $tommy->hears('(^Complaint)','App\Messages\TelegramMessages@complaints');
 
         $tommy->fallback(function(Botman $tom) {
             $tom->typesAndWaits(3);
@@ -137,23 +126,56 @@ class BotManController extends Controller
      **/
     public function facebook()
     {
-        $tommy = resolve('botman');
-        $tommy->hears('(^Hi|Hello)','App\Messages\TelegramMessages@firstMessage');
-        $tommy->hears('Hi',function(BotMan $tom){
-            $tom->reply("Hi my name is Tommy. What's yours");
-        });
-        $tommy->hears('{name}',function(BotMan $tom,$name){
-            //save name in session Maybe?!
-            $tom->reply("Hey "+$name+". Your wish is my command");
-        });
+        $tommy = $this->loadConfig('facebook');
+        $tommy->hears('(^Hi|Hello)','App\Messages\FacebookMessages@firstMessage');
 
-        $tommy->hears('Thank you tommy',function(BotMan $tom){
+        $tommy->hears('GET_STARTED','App\Messages\FacebookMessages@firstMessage');
+        
+        $tommy->hears('tellmemore','App\Messages\FacebookMessages@tellMeMore');
+
+        $tommy->hears('My name is {name}','App\Messages\TelegramMessages@nameMessage');
+
+        $tommy->hears('((?i)\\offers\\b)','App\Messages\FacebookMessages@Offers');
+        $tommy->hears('((?i)\\offer\\b)','App\Messages\FacebookMessages@Offers');
+        $tommy->hears('(^.*offers.*$)','App\Messages\FacebookMessages@Offers');
+
+        $tommy->hears('(^1)','App\Messages\FacebookMessages@complaints');
+        $tommy->hears('(^2)','App\Messages\FacebookMessages@offers');
+        $tommy->hears('(^Complaint)','App\Messages\FacebookMessages@complaints');
+
+        $tommy->hears('(^Complaints)','App\Messages\FacebookMessages@complaints');
+
+        $tommy->hears('(^3)',function(BotMan $tommy){
+            $tommy->typesAndWaits(3);
+            //put cache flag for dialog flow
+            Cache::put('dialogflow_flag_facebook', true, now()->addMinute(20));
+            $tommy->reply("Correct! Let's go there");
+        });
+        
+        //check if chat cache is set
+        if(Cache::has('dialogflow_flag_facebook')){
+            // Apply matching middleware per hears command
+            $dialogflow = ApiAi::create(env('DIALOGFLOW_TOKEN'))->listenForAction();
+            $tommy->middleware->received($dialogflow); 
+            $tommy->hears('smalltalk(.*)', function (BotMan $tom) {
+                // The incoming message matched the "my_api_action" on Dialogflow
+                // Retrieve Dialogflow information:
+                $extras = $tom->getMessage()->getExtras();
+                $apiReply = $extras['apiReply'];
+                $apiAction = $extras['apiAction'];
+                $apiIntent = $extras['apiIntent'];            
+                $tom->reply($apiReply);
+            })->middleware($dialogflow);
+        }
+
+        $tommy->hears('Thank you|Thanks',function(BotMan $tom){
             $tom->reply(ButtonTemplate::create('Do you want to know more about 234BET?')               
-                ->addButton(ElementButton::create('Show Me the Money!')
+                ->addButton(ElementButton::create('Show Me the Money! 😁')
                     ->url('http://www.234bet.com/')
                 )
             );
         });
+
         $tommy->hears('(Goodbye)',function(BotMan $tom){
             $tom->reply(ButtonTemplate::create('Rate our service')
                     ->addButton(ElementButton::create('Excellent')
@@ -168,16 +190,16 @@ class BotManController extends Controller
                     )
                 );
         });
-        $tommy->hears('(^R-)',function(BotMan $tom,$rate){
-            $rate = explode('-',$rate);
-            $rate = $rate[1];
+
+        $tommy->hears('(^R-)',function(BotMan $tom){            
             //persist rating anywhere you want
             $tom->reply('Thank you for rating us. We are absolutely committed to serving you better');
         });
 
-        //add logic for facebook messenger bot
-        //handling and listening for user issues
-        $this->dialogFlow();
+        $tommy->fallback(function(Botman $tom) {
+            $tom->typesAndWaits(3);
+            $tom->reply("Sorry, I did not understand these commands. Here is a list of commands I understand: \n 1 for Complaints \n 2 For our  latest offers. \n 3 Make we yarn wella");
+        });
         
         $tommy->listen();
     }
@@ -191,7 +213,7 @@ class BotManController extends Controller
             case 'telegram':
                     DriverManager::loadDriver(\BotMan\Drivers\Telegram\TelegramDriver::class);
                     $this->config = ['telegram' => [
-                            'token' => '744176429:AAEsmV691fVmbm0E-qB_KqxorWF_I_uF2b8',
+                            'token' =>env('TELEGRAM_TOKEN'),
                         ]
                     ];
                     return BotManFactory::create($this->config,new LaravelCache);
@@ -199,10 +221,10 @@ class BotManController extends Controller
             case 'facebook':
                 DriverManager::loadDriver(\BotMan\Drivers\Telegram\FacebookDriver::class);
                 $this->config = ['facebook' => [
-                        'token' => '',
+                        'token' => env('FACEBOOK_TOKEN'),
                     ]
                 ];
-                return BotManFactory::create($this->config,new RedisCache('127.0.0.1', 6379));
+                return BotManFactory::create($this->config,new laravelCache);
             break;            
             default:
                 # code...
